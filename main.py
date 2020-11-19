3ac285c592bb716a2572a61a2715c7702b17a9fd
rajeevsingla780 <63499957+rajeevsingla780@users.noreply.github.com>
2020-11-19 23:11:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import pyttsx3 
-  
+from gtts import gTTS 
+from playsound import playsound
+import os
 def fun(t):
-    engine = pyttsx3.init() 
-    
-    # testing 
-    engine.setProperty('rate',120)
-    engine.setProperty('volume',0.9)
-    engine.say(str(t))
-   
-    engine.runAndWait()
+    mytext = str(t)
+  
+    language = 'en'
+
+    myobj = gTTS(text=mytext, lang=language, slow=False) 
+
+    myobj.save("welcome.mp3") 
+
+    playsound("welcome.mp3")
+    os.remove('welcome.mp3')
+ 
